e73cc7608d5b81cd94fa628649998ffac7f07d82
Islom Pardaboyev <ipardaboyev574@gmail.com>
2024-10-16 15:18:42 +0500

    init

--- src/context/Context.tsx
+++ src/context/Context.tsx
@@ -9,11 +9,7 @@ export const Context = createContext<ContextType | undefined>(undefined);
 
 export const MainContext = ({ children }: { children: React.ReactNode }) => {
   // const [token, setToken] = useState<boolean>(false)
-  const [token, setToken] = useState<boolean>(
-    window.localStorage.getItem("accessToken") !== null
-      ? JSON.parse(window.localStorage.getItem("accessToken")!)
-      : false
-  );
+  const [token, setToken] = useState<boolean>(window.localStorage.getItem('accessToken') !== null ? true : false);
   return (
     <Context.Provider value={{ token, setToken }}>{children}</Context.Provider>
   );
